8dbf7213affe4c69780d8d2a09c05c55044e9c6b
AdrianaLins <adrianapecorelli.ap@gmail.com>
2021-08-16 18:15:24 -0300

    Feign client

--- hr-payroll/src/main/java/com/devsuperior/payroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/devsuperior/payroll/config/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 	
+	//registra uma instancia única que fica disponível para o projeto e injetar em outros componentes
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
