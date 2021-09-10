6397dec026d70a9590c3d61cfc2d46ab52d9a648
AdrianaLins <adrianapecorelli.ap@gmail.com>
2021-09-10 10:01:53 -0300

    Login e geração do Token JWT

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -20,8 +20,13 @@ public class HrUserApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("BCRYPT: " + pass.encode("123456") );
+		System.out.println("BCRYPT: " + pass.encode("123456"));
 		
+	}
+	
+	
+	public String getPassword() {
+		return null;
 	}
 
 }
